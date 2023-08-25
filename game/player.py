865af3666cc3bf988dc43dabd5161e21671f4753
Elio Valentino Anci <e.anci@alumno.um.edu.ar>
2023-08-25 19:26:13 -0300

    Add give_tiles and change_tiles methods

--- game/player.py
+++ game/player.py
@@ -6,3 +6,13 @@ class Player:
         self.number = number
         self.points = points
         self.lectern = []
+
+    def give_tiles(self, tiles=[]):
+        self.lectern.extend(tiles)
+
+    def change_tiles(self, old_tiles_index=[], new_tiles=[]):
+        old_tiles = []
+        for i in range(len(old_tiles_index)):
+            old_tiles.append(self.lectern[old_tiles_index[i]-1])
+            self.lectern[old_tiles_index[i]-1] = new_tiles[i]
+        return old_tiles
--- tests/test_player.py
+++ tests/test_player.py
@@ -1,11 +1,36 @@
 import unittest
 from game.player import Player, LECTERN_SIZE
+from game.bagtiles import BagTiles
 from unittest.mock import patch
 
 class TestPlayer(unittest.TestCase):
     def test_player_creation(self):
-        player = Player('Isaac', 1, 3)
+        player = Player(name='Isaac', number=1, points=3)
         self.assertEqual(player.name, 'Isaac')
         self.assertEqual(player.number, 1)
         self.assertEqual(player.points, 3)
         self.assertEqual(player.lectern, [])
+
+    def test_player_creation_no_parameters(self):
+        player = Player()
+        self.assertEqual(player.name, '')
+        self.assertEqual(player.number, 0)
+        self.assertEqual(player.points, 0)
+        self.assertEqual(player.lectern, [])
+
+    def test_give_tiles(self):
+        player = Player()
+        player.give_tiles(['A', 'B'])
+        self.assertEqual(player.lectern, ['A', 'B'])
+
+    def test_give_tiles_from_bag(self):
+        bag = BagTiles()
+        player = Player()
+        player.give_tiles(bag.take(7))
+        self.assertEqual(len(player.lectern), 7)
+
+    def test_change_tiles(self):
+        player = Player()
+        player.give_tiles(['A', 'B', 'C'])
+        player.change_tiles(old_tiles_index=[2,3], new_tiles=['Z', 'Y'])
+        self.assertEqual(player.lectern, ['A', 'Z', 'Y'])
