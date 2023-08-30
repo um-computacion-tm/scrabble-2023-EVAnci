9df597ef3a45b161f1581e04a5636a09608f843f
Elio Valentino Anci <e.anci@alumno.um.edu.ar>
2023-08-30 10:46:08 -0300

    Add active attribute

--- game/cell.py
+++ game/cell.py
@@ -1,9 +1,10 @@
 from game.bagtiles import Tile
 
 class Cell:
-    def __init__(self, multiplier:int, letter_multiplier:bool):
+    def __init__(self, multiplier:int, letter_multiplier:bool, active=True):
         self.multiplier = multiplier
         self.letter_multiplier = letter_multiplier
+        self.active = active
         self.letter = None
 
     def add_letter(self, letter:Tile):
@@ -12,7 +13,7 @@ class Cell:
     def calculate_value(self):
         if self.letter is None:
             return 0
-        if self.letter_multiplier:
+        if self.letter_multiplier and self.active:
             return self.letter.value * self.multiplier
         else:
             return self.letter.value
