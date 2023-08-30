940c5799002336914a27eea372a1a5a02b51b5e2
Elio Valentino Anci <e.anci@alumno.um.edu.ar>
2023-08-30 10:43:29 -0300

    Add calculate_word_value method

--- game/board.py
+++ game/board.py
@@ -3,3 +3,9 @@ from game.cell import Cell
 class Board():
     def __init__(self):
         self.grid = [[Cell(1, '') for _ in range(15)] for _ in range(15)]
+
+    def calculate_word_value(self, word):
+        points = 0
+        for cell in word:
+            points += cell.calculate_value()
+        return points
