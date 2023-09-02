2cde05f5c5acbea9213eb99ec4f7a5af6938dcbd
Elio Valentino Anci <e.anci@alumno.um.edu.ar>
2023-09-02 18:18:33 -0300

    test for cli module

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -33,15 +33,11 @@ class TestCliValidate(unittest.TestCase):
     def test_range_input_invalid(self, mock_input):
         with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
             value = range_input(from_value=1, to_value=4, message="Ingrese un número entre 1 y 4: ")
-        print_output = mock_stdout.getvalue()[:-1]
         self.assertEqual(value, 3)
-        self.assertEqual(print_output, 'Valor Invalido...')
 
 class TestCliMenu(unittest.TestCase):
     def test_menu_print(self):
-        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
-            menu(type='menu')
-        print_output = mock_stdout.getvalue()[:-1]
+        print_output = menu(type='menu')
         self.assertEqual(
             print_output,
             '''    Menu
@@ -53,9 +49,7 @@ Seleccion: '''
         )
 
     def test_submenu_print(self):
-        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
-            menu(type='submenu')
-        print_output = mock_stdout.getvalue()[:-1]
+        print_output = menu(type='submenu')
         self.assertEqual(
             print_output,
             '''|___  1) Introducir Palabra
@@ -65,6 +59,46 @@ Seleccion: '''
 |___  5) Volver
 Seleccion: '''
         )
+
+    @patch('builtins.input', side_effect=[1])
+    def test_menu_nav1(self, mock_input):
+        result = nav()
+        self.assertEqual(result, 'board')
+
+    @patch('builtins.input', side_effect=[2])
+    def test_menu_nav2(self, mock_input):
+        result = nav()
+        self.assertEqual(result, 'lectern')
+
+    @patch('builtins.input', side_effect=[3,1])
+    def test_menu_nav31(self, mock_input):
+        result = nav()
+        self.assertEqual(result, 'play')
+
+    @patch('builtins.input', side_effect=[3,2])
+    def test_menu_nav32(self, mock_input):
+        result = nav()
+        self.assertEqual(result, 'change')
+
+    @patch('builtins.input', side_effect=[3,3])
+    def test_menu_nav33(self, mock_input):
+        result = nav()
+        self.assertEqual(result, 'pass')
+
+    @patch('builtins.input', side_effect=[3,4])
+    def test_menu_nav34(self, mock_input):
+        result = nav()
+        self.assertEqual(result, 'giveup')
+
+    @patch('builtins.input', side_effect=[3,5,1])
+    def test_menu_nav35(self, mock_input):
+        result = nav()
+        self.assertEqual(result, 'board')
+
+    @patch('builtins.input', side_effect=[4])
+    def test_menu_nav4(self, mock_input):
+        result = nav()
+        self.assertEqual(result, 'points')
 
 if __name__ == '__main__':
     unittest.main()
